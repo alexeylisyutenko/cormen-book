1fd2841afb6d8b98dd4daca64a6652af025acd48
Alexey Lisyutenko <alis@icbcom.ru>
2019-06-02 18:22:31 +0300

    Try to implement parallel quick sort using AtomicIntegerArray. Performance got worse

--- src/main/java/ru/alexeylisyutenko/cormen/chapter7/QuickSortParallel.java
+++ src/main/java/ru/alexeylisyutenko/cormen/chapter7/QuickSortParallel.java
@@ -4,27 +4,29 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
-import static ru.alexeylisyutenko.helper.Helpers.exchange;
-
 public class QuickSortParallel {
 
     public static void sort(int[] array) {
-//        AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(array);
-//        atomicIntegerArray.get
+        AtomicIntegerArray atomicIntegerArray = new AtomicIntegerArray(array);
+        ForkJoinPool.commonPool().invoke(new QuickSortRecursiveAction(atomicIntegerArray, 0, array.length - 1));
+        copyAtomicIntegerArrayToSourceArray(array, atomicIntegerArray);
+    }
 
-
-        ForkJoinPool.commonPool().invoke(new QuickSortRecursiveAction(array, 0, array.length - 1));
+    private static void copyAtomicIntegerArrayToSourceArray(int[] array, AtomicIntegerArray atomicIntegerArray) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = atomicIntegerArray.get(i);
+        }
     }
 
     private static class QuickSortRecursiveAction extends RecursiveAction {
         private static final int THRESHOLD = 1 << 13;
 
-        private final int[] array;
+        private final AtomicIntegerArray atomicIntegerArray;
         private final int p;
         private final int r;
 
-        QuickSortRecursiveAction(int[] array, int p, int r) {
-            this.array = array;
+        QuickSortRecursiveAction(AtomicIntegerArray atomicIntegerArray, int p, int r) {
+            this.atomicIntegerArray = atomicIntegerArray;
             this.p = p;
             this.r = r;
         }
@@ -32,48 +34,42 @@ public class QuickSortParallel {
         @Override
         protected void compute() {
             if (r - p < THRESHOLD) {
-                synchronizedDirectQuickSort();
+                directQuickSort(p, r);
             } else {
-                int q = synchronizedPartition();
-                QuickSortRecursiveAction task1 = new QuickSortRecursiveAction(array, p, q - 1);
-                QuickSortRecursiveAction task2 = new QuickSortRecursiveAction(array, q + 1, r);
+                int q = partition(p, r);
+                QuickSortRecursiveAction task1 = new QuickSortRecursiveAction(atomicIntegerArray, p, q - 1);
+                QuickSortRecursiveAction task2 = new QuickSortRecursiveAction(atomicIntegerArray, q + 1, r);
                 task1.fork();
                 task2.compute();
                 task1.join();
             }
         }
 
-        private void synchronizedDirectQuickSort() {
-            synchronized (array) {
-                directQuickSort(array, p, r);
-            }
-        }
-
-        private int synchronizedPartition() {
-            synchronized (array) {
-                return partition(array, p, r);
-            }
-        }
-
-        private void directQuickSort(int[] array, int p, int r) {
+        private void directQuickSort(int p, int r) {
             while (p < r) {
-                int q = partition(array, p, r);
-                directQuickSort(array, p, q - 1);
+                int q = partition(p, r);
+                directQuickSort(p, q - 1);
                 p = q + 1;
             }
         }
 
-        private int partition(int[] array, int p, int r) {
-            int x = array[r];
+        private int partition(int p, int r) {
+            int x = atomicIntegerArray.get(r);
             int i = p - 1;
             for (int j = p; j < r; j++) {
-                if (array[j] <= x) {
+                if (atomicIntegerArray.get(j) <= x) {
                     i++;
-                    exchange(array, i, j);
+                    exchange(i, j);
                 }
             }
-            exchange(array, i + 1, r);
+            exchange(i + 1, r);
             return i + 1;
+        }
+
+        private void exchange(int firstIndex, int secondIndex) {
+            int temp = atomicIntegerArray.get(firstIndex);
+            atomicIntegerArray.set(firstIndex, atomicIntegerArray.get(secondIndex));
+            atomicIntegerArray.set(secondIndex, temp);
         }
 
     }
