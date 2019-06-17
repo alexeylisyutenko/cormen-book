d087a3550a6cd620d042ace5b2ad6ee8014ef6ed
Alexey Lisyutenko <alis@icbcom.ru>
2019-06-18 02:48:44 +0300

    Finish development of MedianOfTwoSortedArrays

--- src/main/java/ru/alexeylisyutenko/cormen/chapter9/MedianOfTwoSortedArrays.java
+++ src/main/java/ru/alexeylisyutenko/cormen/chapter9/MedianOfTwoSortedArrays.java
@@ -9,22 +9,42 @@ public class MedianOfTwoSortedArrays {
         if (array1.length == 0) {
             throw new IllegalArgumentException("Each arrays must contain at least one element");
         }
-        return findMedianRecursive(array1, array2, 0, array1.length - 1, 0, array2.length - 1);
+        return doFindMedian(array1, array2, 0, array1.length - 1, 0, array2.length - 1);
     }
 
-    private static int findMedianRecursive(int[] array1, int[] array2, int lo1, int hi1, int lo2, int hi2) {
-        int n = hi1 - lo1 + 1;
-        if (n == 1) {
-            return Math.max(array1[lo1], array2[lo1]);
-        }
+    private static int doFindMedian(int[] array1, int[] array2, int lo1, int hi1, int lo2, int hi2) {
+        while (true) {
+            int n = hi1 - lo1 + 1;
 
-        int k = n / 2;
+            if (n == 1) {
+                return Math.min(array1[lo1], array2[lo2]);
+            } else if (n == 2) {
+                return Math.min(Math.max(array1[lo1], array2[lo2]), Math.min(array1[hi1], array2[hi2]));
+            }
 
-        //!!!
-        if (array1[lo1 + k - 1] < array2[lo2 + k - 1]) {
-            return findMedianRecursive(array1, array2, lo1 + k - 1, hi1, lo2, lo2 + k);
-        } else {
-            return findMedianRecursive(array1, array2, lo1, lo1 + k - 1, lo2 + k, hi2);
+            int medianIndex = (n + 1) / 2 - 1;
+            int median1 = array1[lo1 + medianIndex];
+            int median2 = array2[lo2 + medianIndex];
+
+            if (median1 == median2) {
+                return median1;
+            } else if (median1 < median2) {
+                if (n % 2 == 0) {
+                    lo1 = lo1 + medianIndex + 1;
+                    hi2 = lo2 + medianIndex;
+                } else {
+                    lo1 = lo1 + medianIndex;
+                    hi2 = lo2 + medianIndex;
+                }
+            } else {
+                if (n % 2 == 0) {
+                    hi1 = lo1 + medianIndex;
+                    lo2 = lo2 + medianIndex + 1;
+                } else {
+                    hi1 = lo1 + medianIndex;
+                    lo2 = lo2 + medianIndex;
+                }
+            }
         }
 
     }
--- src/test/java/ru/alexeylisyutenko/cormen/chapter9/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ru/alexeylisyutenko/cormen/chapter9/MedianOfTwoSortedArraysTest.java
@@ -4,22 +4,36 @@ import org.junit.jupiter.api.Test;
 import ru.alexeylisyutenko.helper.Helpers;
 
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.alexeylisyutenko.cormen.chapter9.MedianOfTwoSortedArrays.findMedian;
 
 class MedianOfTwoSortedArraysTest {
 
     @Test
     void findMedianDemo() {
+//        int size = ThreadLocalRandom.current().nextInt(1, 10000);
+//        int[] array1 = Helpers.randomPositiveIntArray(size, size * 100);
+//        Arrays.sort(array1);
+//        int[] array2 = Helpers.randomPositiveIntArray(size, size * 100);
+//        Arrays.sort(array2);
+
         int[] array1 = {1, 3, 5, 7};
         int[] array2 = {1, 4, 6, 8};
 
-//        int[] array1 = {77, 157, 195, 326};
-//        int[] array2 = {59, 80, 132, 169};
+//        int[] array1 = {107, 148, 203, 281, 367, 451};
+//        int[] array2 = {141, 204, 257, 303, 384, 554};
+
+//        int[] array1 = {99};
+//        int[] array2 = {74};
+
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
+
+        int[] mergedArray = mergeArrays(array1, array2);
+        System.out.println(Arrays.toString(mergedArray));
 
         int median = findMedian(array1, array2);
         System.out.println(median);
@@ -27,25 +41,41 @@ class MedianOfTwoSortedArraysTest {
 
     @Test
     void findMedianShouldWorkProperly() {
-        int size = 2;
-        int[] array1 = Helpers.randomPositiveIntArray(size, size * 100);
-        Arrays.sort(array1);
-        int[] array2 = Helpers.randomPositiveIntArray(size, size * 100);
-        Arrays.sort(array2);
+        for (int i = 0; i < 100000; i++) {
+            int size = ThreadLocalRandom.current().nextInt(1, 100);
+            int[] array1 = Helpers.randomPositiveIntArray(size, size * 100);
+            Arrays.sort(array1);
+            int[] array2 = Helpers.randomPositiveIntArray(size, size * 100);
+            Arrays.sort(array2);
 
-        System.out.println(Arrays.toString(array1));
-        System.out.println(Arrays.toString(array2));
 
+            int[] mergedArray = mergeArrays(array1, array2);
+            int expectedMedian = mergedArray[size - 1];
+
+            int median = findMedian(array1, array2);
+
+
+            String message = String.format("array1: %s, array2: %s, expected: %d, actual: %d", Arrays.toString(array1),
+                    Arrays.toString(array2), expectedMedian, median);
+            if (expectedMedian != median) {
+                System.out.println(Arrays.toString(array1));
+                System.out.println(Arrays.toString(array2));
+                System.out.println(Arrays.toString(mergedArray));
+                System.out.println("Expected: " + expectedMedian);
+                System.out.println("Actual: " + median);
+            }
+
+            assertEquals(expectedMedian, median, message);
+        }
+    }
+
+    private int[] mergeArrays(int[] array1, int[] array2) {
+        int size = array1.length;
         int[] mergedArray = new int[size * 2];
         System.arraycopy(array1, 0, mergedArray, 0, size);
         System.arraycopy(array2, 0, mergedArray, size, size);
         Arrays.sort(mergedArray);
-        int expectedMedian = mergedArray[size - 1];
-
-        System.out.println("Exected median: " + expectedMedian);
-
-        int median = findMedian(array1, array2);
-        assertEquals(expectedMedian, median);
+        return mergedArray;
     }
 
 }
