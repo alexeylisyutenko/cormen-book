c18c5f34fd2bfbec85abb4c8b9f7a02371b4ccbe
Alexey Lisyutenko <alis@icbcom.ru>
2019-07-26 15:09:00 +0300

    Implement randomizedHashTableTest

--- src/test/java/ru/alexeylisyutenko/cormen/chapter11/builtinchaininghashtable/BuiltInChainingHashTableTest.java
+++ src/test/java/ru/alexeylisyutenko/cormen/chapter11/builtinchaininghashtable/BuiltInChainingHashTableTest.java
@@ -1,11 +1,17 @@
 package ru.alexeylisyutenko.cormen.chapter11.builtinchaininghashtable;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Test;
 import ru.alexeylisyutenko.cormen.chapter11.HashTable;
 import ru.alexeylisyutenko.cormen.chapter11.hashfunctionfactory.DivisionMethodIntegerHashFunctionFactory;
-import ru.alexeylisyutenko.cormen.chapter11.hashfunctionfactory.MultiplicationMethodIntegerHashFunctionFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class BuiltInChainingHashTableTest {
 
@@ -37,6 +43,76 @@ class BuiltInChainingHashTableTest {
 
         hashTable.delete(1);
         System.out.println(hashTable);
+    }
+
+    @Test
+    void randomizedHashTableTest() {
+        int testCount = 100000;
+        for (int i = 0; i < testCount; i++) {
+            singleRandomizedTest();
+        }
+    }
+
+    private void singleRandomizedTest() {
+        int hashTableSize = RandomUtils.nextInt(1, 256);
+        HashTable<Integer, String> hashTable = new BuiltInChainingHashTable<>(new DivisionMethodIntegerHashFunctionFactory(), hashTableSize);
+
+        // Insert n random items.
+        int numberOfItemsToInsert = RandomUtils.nextInt((hashTableSize / 2) + 1, hashTableSize + 1);
+        List<Integer> keys = generateRandomDistinctIntegers(numberOfItemsToInsert);
+        for (Integer integer : keys) {
+            hashTable.insert(integer, integer + "");
+        }
+
+        // Check that all inserted items can be successfully searched.
+        for (Integer key : keys) {
+            assertEquals(key + "", hashTable.search(key));
+        }
+
+        // Select random elements to delete from the table.
+        Set<Integer> indexesOfNumbersToDelete = getIndexesOfNumbersToDelete(keys.size());
+        List<Integer> keysToDelete = new ArrayList<>();
+        List<Integer> keysToKeep = new ArrayList<>();
+        for (int i = 0; i < keys.size(); i++) {
+            if (indexesOfNumbersToDelete.contains(i)) {
+                keysToDelete.add(keys.get(i));
+            } else {
+                keysToKeep.add(keys.get(i));
+            }
+        }
+
+        // Delete some items from the hash table.
+        for (Integer keyToDelete : keysToDelete) {
+            hashTable.delete(keyToDelete);
+        }
+
+        // Check that deleted keys cannot be searched.
+        for (Integer key : keysToDelete) {
+            assertNull(hashTable.search(key));
+        }
+
+        // Check that not deleted keys still can be searched
+        for (Integer key : keysToKeep) {
+            assertEquals(key + "", hashTable.search(key));
+        }
+    }
+
+    private List<Integer> generateRandomDistinctIntegers(int count) {
+        HashSet<Integer> integerHashSet = new HashSet<>();
+        for (int i = 0; i < count; i++) {
+            int randomInt;
+            do {
+                randomInt = RandomUtils.nextBoolean() ? RandomUtils.nextInt() : -RandomUtils.nextInt();
+            } while (integerHashSet.contains(randomInt));
+            integerHashSet.add(randomInt);
+        }
+        return new ArrayList<>(integerHashSet);
+    }
+
+    private Set<Integer> getIndexesOfNumbersToDelete(int numberOfItemsToInsert) {
+        List<Integer> indexes = IntStream.range(0, numberOfItemsToInsert).boxed().collect(toList());
+        Collections.shuffle(indexes);
+        return indexes.stream().limit(numberOfItemsToInsert / 2).collect(Collectors.toSet());
     }
 
 
