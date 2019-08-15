424455412d0481e402e7682f40c70c55cf1ad70b
Alexey Lisyutenko <alis@icbcom.ru>
2019-08-15 17:18:01 +0300

    Add CompleteBinarySearchTreeAverageDepth calculation

---
+++ src/test/java/ru/alexeylisyutenko/cormen/chapter12/other/CompleteBinarySearchTreeAverageDepth.java
@@ -0,0 +1,38 @@
+package ru.alexeylisyutenko.cormen.chapter12.other;
+
+import org.junit.jupiter.api.Test;
+
+public class CompleteBinarySearchTreeAverageDepth {
+
+    @Test
+    void calculateAverageDepth() {
+        int numberOfNodes = (1 << 3) - 1;
+        System.out.println("Number of nodes:" + numberOfNodes);
+
+        int height = (int) Math.floor(Math.log(numberOfNodes) / Math.log(2));
+        System.out.println("Height: " + height);
+
+        long sumOfDepths = 0;
+
+        int levels = height + 1;
+        for (int i = 0; i < levels; i++) {
+            sumOfDepths = sumOfDepths + (i * (1 << i));
+        }
+
+        double average = (double) sumOfDepths / numberOfNodes;
+        System.out.println("Average depth: " + average);
+        System.out.println("Upper bound on average depth: " + calculateUpperBound(numberOfNodes));
+    }
+
+    private double calculateUpperBound(int numberOfNodes) {
+        int height = (int) Math.floor(Math.log(numberOfNodes) / Math.log(2));
+
+        double p1 = (height + 1) * (1 << (height + 1)) / Math.log(2);
+        double p2 = (1 << (height + 1)) / (Math.log(2) * Math.log(2));
+        double p3 = 1 / (Math.log(2) * Math.log(2));
+
+        double bound = (p1 - p2 + p3) / numberOfNodes;
+
+        return bound;
+    }
+}
