c77232ec98a9564d86ede636cef100adba7ef834
Alexey Lisyutenko <firely3000@gmail.com>
2020-03-05 03:05:56 +0300

    Continue Cormen 15.1 - RodCutting section development

--- src/main/java/ru/alexeylisyutenko/cormen/chapter15/rodcutting/RodCutting.java
+++ src/main/java/ru/alexeylisyutenko/cormen/chapter15/rodcutting/RodCutting.java
@@ -1,9 +1,16 @@
 package ru.alexeylisyutenko.cormen.chapter15.rodcutting;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Arrays;
+
+import static org.apache.commons.lang3.math.NumberUtils.max;
+
 /**
  * Cormen. 15.1 Rod Cutting.
  */
-public interface RodCutting {
+public class RodCutting {
+
     /**
      * Recursive top-down implementation.
      *
@@ -11,5 +18,138 @@ public interface RodCutting {
      * @param size   size of a rod
      * @return best possible revenue
      */
-    int cutRod(int[] prices, int size);
+    public static int cutRod(int[] prices, int size) {
+        if (size == 0) {
+            return 0;
+        }
+        int maxRevenue = Integer.MIN_VALUE;
+        for (int i = 1; i <= size; i++) {
+            maxRevenue = max(maxRevenue, prices[i - 1] + cutRod(prices, size - i));
+        }
+        return maxRevenue;
+    }
+
+    /**
+     * Memoized version of the algorithm.
+     *
+     * @param prices price list
+     * @param size   size of a rod
+     * @return best possible revenue
+     */
+    public static int memoizedCutRod(int[] prices, int size) {
+        int[] revenueCache = new int[size + 1];
+        Arrays.fill(revenueCache, Integer.MIN_VALUE);
+        return memoizedCutRodAux(prices, size, revenueCache);
+    }
+
+    private static int memoizedCutRodAux(int[] prices, int size, int[] revenueCache) {
+        if (revenueCache[size] >= 0) {
+            return revenueCache[size];
+        }
+        int maxRevenue = Integer.MIN_VALUE;
+        if (size == 0) {
+            maxRevenue = 0;
+        } else {
+            for (int i = 1; i <= size; i++) {
+                maxRevenue = max(maxRevenue, prices[i - 1] + memoizedCutRodAux(prices, size - i, revenueCache));
+            }
+        }
+        revenueCache[size] = maxRevenue;
+        return maxRevenue;
+    }
+
+    /**
+     * Bottom-up version of the algorithm.
+     *
+     * @param prices price list
+     * @param size   size of a rod
+     * @return best possible revenue
+     */
+    public static int bottomUpCutRod(int[] prices, int size) {
+        int[] revenues = new int[size + 1];
+        revenues[0] = 0;
+        for (int j = 1; j <= size; j++) {
+            int maxRevenue = Integer.MIN_VALUE;
+            for (int i = 1; i <= j; i++) {
+                maxRevenue = max(maxRevenue, prices[i - 1] + revenues[j - i]);
+            }
+            revenues[j] = maxRevenue;
+        }
+        return revenues[size];
+    }
+
+    /**
+     * Bottom-up version of the algorithm which returns the optimal split along with the best revenue.
+     *
+     * @param prices price list
+     * @param size   size of a rod
+     * @return best possible revenue and best split
+     */
+    public static Pair<Integer, int[]> extendedBottomUpCutRod(int[] prices, int size) {
+        int[] revenues = new int[size + 1];
+        int[] split = new int[size];
+        revenues[0] = 0;
+        for (int j = 1; j <= size; j++) {
+            int maxRevenue = Integer.MIN_VALUE;
+            for (int i = 1; i <= j; i++) {
+                int revenue = prices[i - 1] + revenues[j - i];
+                if (maxRevenue < revenue) {
+                    maxRevenue = revenue;
+                    split[j - 1] = i;
+                }
+            }
+            revenues[j] = maxRevenue;
+        }
+        return Pair.of(revenues[size], split);
+    }
+
+    /**
+     * Helper method that builds a string with a rod split representation inside.
+     *
+     * @param split split
+     * @param size size of original rod
+     * @return string with a rod split representation inside
+     */
+    public static String splitToString(int[] split, int size) {
+        if (split.length != size) {
+            throw new IllegalArgumentException("split.length must be equal to size");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (size > 0) {
+            sb.append(split[size - 1]);
+            size = size - split[size - 1];
+            if (size > 0) {
+                sb.append(" + ");
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Recursive top-down implementation. Which also consider cost of a single cut.
+     *
+     * @param prices price list
+     * @param cost   cost of a single cut
+     * @param size   size of a rod
+     * @return best possible revenue
+     */
+    public static int cutRoadWithCost(int[] prices, int cost, int size) {
+        if (size == 0) {
+            return 0;
+        }
+        int maxRevenue = Integer.MIN_VALUE;
+        for (int i = 1; i <= size; i++) {
+            int currentCost = i == size ? 0 : cost;
+            int revenue = prices[i - 1] + cutRoadWithCost(prices, cost, size - i) - currentCost;
+            if (maxRevenue < revenue) {
+                maxRevenue = revenue;
+            }
+        }
+        return maxRevenue;
+    }
+
+    public static Pair<Integer, int[]> extendedBottomUpCutRodWithCost(int[] prices, int size) {
+        throw new IllegalStateException("Not implemented yet");
+    }
 }
---
+++ src/test/java/ru/alexeylisyutenko/cormen/chapter15/rodcutting/RodCuttingTest.java
@@ -0,0 +1,90 @@
+package ru.alexeylisyutenko.cormen.chapter15.rodcutting;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.alexeylisyutenko.cormen.chapter15.rodcutting.RodCutting.splitToString;
+
+class RodCuttingTest {
+
+    public static final int[] DEFAULT_PRICES = new int[]{1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+
+    @Test
+    void demo() {
+        int size = 5;
+        System.out.println(RodCutting.cutRod(DEFAULT_PRICES, size));
+        System.out.println(RodCutting.cutRoadWithCost(DEFAULT_PRICES, 3, size));
+    }
+
+    @Test
+    void demo2() {
+        int size = 7;
+        Pair<Integer, int[]> revenueAndSplit = RodCutting.extendedBottomUpCutRod(DEFAULT_PRICES, size);
+        System.out.println("Revenue: " + revenueAndSplit.getLeft() + ",\tsplit: " + splitToString(revenueAndSplit.getRight(), size));
+    }
+
+    @Test
+    void cutRod() {
+        assertEquals(1, RodCutting.cutRod(DEFAULT_PRICES, 1));
+        assertEquals(5, RodCutting.cutRod(DEFAULT_PRICES, 2));
+        assertEquals(8, RodCutting.cutRod(DEFAULT_PRICES, 3));
+        assertEquals(10, RodCutting.cutRod(DEFAULT_PRICES, 4));
+        assertEquals(13, RodCutting.cutRod(DEFAULT_PRICES, 5));
+        assertEquals(17, RodCutting.cutRod(DEFAULT_PRICES, 6));
+        assertEquals(18, RodCutting.cutRod(DEFAULT_PRICES, 7));
+        assertEquals(22, RodCutting.cutRod(DEFAULT_PRICES, 8));
+        assertEquals(25, RodCutting.cutRod(DEFAULT_PRICES, 9));
+        assertEquals(30, RodCutting.cutRod(DEFAULT_PRICES, 10));
+    }
+
+    @Test
+    void memoizedCutRod() {
+        assertEquals(1, RodCutting.memoizedCutRod(DEFAULT_PRICES, 1));
+        assertEquals(5, RodCutting.memoizedCutRod(DEFAULT_PRICES, 2));
+        assertEquals(8, RodCutting.memoizedCutRod(DEFAULT_PRICES, 3));
+        assertEquals(10, RodCutting.memoizedCutRod(DEFAULT_PRICES, 4));
+        assertEquals(13, RodCutting.memoizedCutRod(DEFAULT_PRICES, 5));
+        assertEquals(17, RodCutting.memoizedCutRod(DEFAULT_PRICES, 6));
+        assertEquals(18, RodCutting.memoizedCutRod(DEFAULT_PRICES, 7));
+        assertEquals(22, RodCutting.memoizedCutRod(DEFAULT_PRICES, 8));
+        assertEquals(25, RodCutting.memoizedCutRod(DEFAULT_PRICES, 9));
+        assertEquals(30, RodCutting.memoizedCutRod(DEFAULT_PRICES, 10));
+    }
+
+    @Test
+    void bottomUpCutRod() {
+        assertEquals(1, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 1));
+        assertEquals(5, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 2));
+        assertEquals(8, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 3));
+        assertEquals(10, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 4));
+        assertEquals(13, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 5));
+        assertEquals(17, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 6));
+        assertEquals(18, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 7));
+        assertEquals(22, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 8));
+        assertEquals(25, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 9));
+        assertEquals(30, RodCutting.bottomUpCutRod(DEFAULT_PRICES, 10));
+    }
+
+    @Test
+    void cutRoadWithCost() {
+        assertEquals(1, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 1));
+        assertEquals(5, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 2));
+        assertEquals(8, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 3));
+        assertEquals(10, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 4));
+        assertEquals(13, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 5));
+        assertEquals(17, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 6));
+        assertEquals(18, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 7));
+        assertEquals(22, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 8));
+        assertEquals(25, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 9));
+        assertEquals(30, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 0, 10));
+
+        assertEquals(11, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 2, 5));
+
+        assertEquals(9, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 10, 4));
+        assertEquals(10, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 10, 5));
+        assertEquals(17, RodCutting.cutRoadWithCost(DEFAULT_PRICES, 10, 7));
+    }
+
+
+}
