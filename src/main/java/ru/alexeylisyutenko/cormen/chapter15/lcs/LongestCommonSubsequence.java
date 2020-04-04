f3a15a3e85e9c2caff8efb5fa27342bc41b11557
Alexey Lisyutenko <firely3000@gmail.com>
2020-04-04 03:28:33 +0300

    Add exercises 15.4-2 and 15.4-3

--- src/main/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestCommonSubsequence.java
+++ src/main/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestCommonSubsequence.java
@@ -1,7 +1,9 @@
 package ru.alexeylisyutenko.cormen.chapter15.lcs;
 
+import com.jakewharton.fliptables.FlipTable;
 import lombok.Value;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -18,7 +20,7 @@ public final class LongestCommonSubsequence {
      * @param sequence2 second sequence
      * @return lengths of subproblems which can be use to reconstruct the resulting longest common subsequence
      */
-    public static LcsLengths lcsLength(String sequence1, String sequence2) {
+    public static LcsLengthsAndSolutions lcsLength(String sequence1, String sequence2) {
         Objects.requireNonNull(sequence1, "sequence1 cannot be null");
         Objects.requireNonNull(sequence2, "sequence2 cannot be null");
 
@@ -26,7 +28,7 @@ public final class LongestCommonSubsequence {
         int n = sequence2.length();
 
         int[][] lengths = new int[m + 1][n + 1];
-        byte[][] chosenSolutions = new byte[m + 1][n + 1];
+        int[][] chosenSolutions = new int[m + 1][n + 1];
 
         for (int i = 1; i <= m; i++) {
             lengths[i][0] = 0;
@@ -50,25 +52,25 @@ public final class LongestCommonSubsequence {
             }
         }
 
-        return new LcsLengths(lengths, chosenSolutions);
+        return new LcsLengthsAndSolutions(lengths, chosenSolutions);
     }
 
     /**
-     * Constructs the longest common subsequence by data from {@link LcsLengths} object.
+     * Constructs the longest common subsequence by data from {@link LcsLengthsAndSolutions} object.
      *
-     * @param lcsLengths lengths of subproblems.
-     * @param sequence1 first sequence
-     * @param m number of elements in the first sequence
-     * @param n number of elements in the second sequence
+     * @param lcsLengthsAndSolutions lengths of subproblems.
+     * @param sequence1              first sequence
+     * @param m                      number of elements in the first sequence
+     * @param n                      number of elements in the second sequence
      * @return the longest common subsequence
      */
-    public static String constructLcs(LongestCommonSubsequence.LcsLengths lcsLengths, String sequence1, int m, int n) {
+    public static String constructLcs(LcsLengthsAndSolutions lcsLengthsAndSolutions, String sequence1, int m, int n) {
         StringBuilder stringBuilder = new StringBuilder();
-        constructLcsAux(lcsLengths.getChosenSolutions(), sequence1, m, n, stringBuilder);
+        constructLcsAux(lcsLengthsAndSolutions.getChosenSolutions(), sequence1, m, n, stringBuilder);
         return stringBuilder.toString();
     }
 
-    private static void constructLcsAux(byte[][] chosenSolutions, String sequence1, int i, int j, StringBuilder stringBuilder) {
+    private static void constructLcsAux(int[][] chosenSolutions, String sequence1, int i, int j, StringBuilder stringBuilder) {
         if (i == 0 || j == 0) {
             return;
         }
@@ -83,6 +85,82 @@ public final class LongestCommonSubsequence {
     }
 
     /**
+     * Exercise 15.4-2.
+     */
+    public static String constructLcsShort(int[][] lengths, String sequence1, String sequence2) {
+        StringBuilder stringBuilder = new StringBuilder();
+        constructLcsShortAux(lengths, sequence1, sequence2, sequence1.length(), sequence2.length(), stringBuilder);
+        return stringBuilder.toString();
+    }
+
+    private static void constructLcsShortAux(int[][] lengths, String sequence1, String sequence2, int i, int j, StringBuilder sb) {
+        if (i == 0 || j == 0) {
+            return;
+        }
+        if (sequence1.charAt(i - 1) == sequence2.charAt(j - 1)) {
+            constructLcsShortAux(lengths, sequence1, sequence2, i - 1, j - 1, sb);
+            sb.append(sequence1.charAt(i - 1));
+        } else if (lengths[i - 1][j] >= lengths[i][j - 1]) {
+            constructLcsShortAux(lengths, sequence1, sequence2, i - 1, j, sb);
+        } else {
+            constructLcsShortAux(lengths, sequence1, sequence2, i, j - 1, sb);
+        }
+    }
+
+
+    /**
+     * Exercise 15.4-3.
+     */
+    public static LcsLengthsAndSolutions lcsLengthsMemoized(String sequence1, String sequence2) {
+        Objects.requireNonNull(sequence1, "sequence1 cannot be null");
+        Objects.requireNonNull(sequence2, "sequence2 cannot be null");
+
+        int m = sequence1.length();
+        int n = sequence2.length();
+
+        int[][] lengths = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                lengths[i][j] = -1;
+            }
+        }
+        int[][] chosenSolutions = new int[m + 1][n + 1];
+        lcsLengthsMemoizedAux(sequence1, sequence2, m, n, lengths, chosenSolutions);
+        return new LcsLengthsAndSolutions(lengths, chosenSolutions);
+    }
+
+    private static int lcsLengthsMemoizedAux(String sequence1, String sequence2, int i, int j, int[][] lengths, int[][] chosenSolutions) {
+        if (lengths[i][j] >= 0) {
+            return lengths[i][j];
+        }
+
+        int length;
+
+        if (i == 0 || j == 0) {
+            length = 0;
+        } else if (sequence1.charAt(i - 1) == sequence2.charAt(j - 1)) {
+            length = lcsLengthsMemoizedAux(sequence1, sequence2, i - 1, j - 1, lengths, chosenSolutions) + 1;
+            chosenSolutions[i][j] = 1;
+        } else {
+            int l1 = lcsLengthsMemoizedAux(sequence1, sequence2, i - 1, j, lengths, chosenSolutions);
+            int l2 = lcsLengthsMemoizedAux(sequence1, sequence2, i, j - 1, lengths, chosenSolutions);
+
+            if (l1 >= l2) {
+                length = l1;
+                chosenSolutions[i][j] = 2;
+            } else {
+                length = l2;
+                chosenSolutions[i][j] = 3;
+            }
+        }
+
+        lengths[i][j] = length;
+        return length;
+    }
+
+    // Print all LCS? How to do that? Can we use information in
+
+    /**
      * Finds the longest common subsequence of two sequences.
      *
      * @param sequence1 first sequence
@@ -90,15 +168,15 @@ public final class LongestCommonSubsequence {
      * @return the longest common subsequence
      */
     public static String findLcs(String sequence1, String sequence2) {
-        LcsLengths lcsLengths = lcsLength(sequence1, sequence2);
-        return constructLcs(lcsLengths, sequence1, sequence1.length(), sequence2.length());
+        LcsLengthsAndSolutions lcsLengthsAndSolutions = lcsLength(sequence1, sequence2);
+        return constructLcs(lcsLengthsAndSolutions, sequence1, sequence1.length(), sequence2.length());
     }
 
     /**
-     * Lengths of subproblems.
+     * Lengths of subproblems along with a helper field used to construct an optimal solution.
      */
     @Value
-    public static class LcsLengths {
+    public static class LcsLengthsAndSolutions {
         /**
          * Lengths of subproblems.
          */
@@ -107,6 +185,33 @@ public final class LongestCommonSubsequence {
         /**
          * This is a helper field used to construct an optimal solution.
          */
-        byte[][] chosenSolutions;
+        int[][] chosenSolutions;
+
+        public String toLengthsTable() {
+            return constructTable(lengths);
+        }
+
+        public String toChosenSolutionsTable() {
+            return constructTable(chosenSolutions);
+        }
+
+        private String constructTable(int[][] source) {
+            int rows = source.length;
+            int columns = (rows == 0) ? 0 : source[0].length;
+            String[] headers = constructEmptyHeader(columns);
+            String[][] data = Arrays.stream(source)
+                    .map(ints -> Arrays.stream(ints).mapToObj(String::valueOf).toArray(String[]::new))
+                    .toArray(String[][]::new);
+            return FlipTable.of(headers, data);
+        }
+
+        private String[] constructEmptyHeader(int columns) {
+            String[] headers = new String[columns];
+            for (int i = 0; i < columns; i++) {
+                headers[i] = "";
+            }
+            return headers;
+        }
+
     }
 }
--- src/test/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestCommonSubsequenceTest.java
+++ src/test/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestCommonSubsequenceTest.java
@@ -1,9 +1,9 @@
 package ru.alexeylisyutenko.cormen.chapter15.lcs;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LongestCommonSubsequenceTest {
 
@@ -18,34 +18,57 @@ class LongestCommonSubsequenceTest {
         String sequence1 = "ABCBDAB";
         String sequence2 = "BDCABA";
 
-        LongestCommonSubsequence.LcsLengths lcsLengths = LongestCommonSubsequence.lcsLength(sequence1, sequence2);
+        LongestCommonSubsequence.LcsLengthsAndSolutions lcsLengthsAndSolutions = LongestCommonSubsequence.lcsLength(sequence1, sequence2);
 
-        assertArrayEquals(new int[] {0, 0, 0, 0, 0, 0, 0}, lcsLengths.getLengths()[0]);
-        assertArrayEquals(new int[] {0, 0, 0, 0, 1, 1, 1}, lcsLengths.getLengths()[1]);
-        assertArrayEquals(new int[] {0, 1, 1, 1, 1, 2, 2}, lcsLengths.getLengths()[2]);
-        assertArrayEquals(new int[] {0, 1, 1, 2, 2, 2, 2}, lcsLengths.getLengths()[3]);
-        assertArrayEquals(new int[] {0, 1, 1, 2, 2, 3, 3}, lcsLengths.getLengths()[4]);
-        assertArrayEquals(new int[] {0, 1, 2, 2, 2, 3, 3}, lcsLengths.getLengths()[5]);
-        assertArrayEquals(new int[] {0, 1, 2, 2, 3, 3, 4}, lcsLengths.getLengths()[6]);
-        assertArrayEquals(new int[] {0, 1, 2, 2, 3, 4, 4}, lcsLengths.getLengths()[7]);
+        assertArrayEquals(new int[]{0, 0, 0, 0, 0, 0, 0}, lcsLengthsAndSolutions.getLengths()[0]);
+        assertArrayEquals(new int[]{0, 0, 0, 0, 1, 1, 1}, lcsLengthsAndSolutions.getLengths()[1]);
+        assertArrayEquals(new int[]{0, 1, 1, 1, 1, 2, 2}, lcsLengthsAndSolutions.getLengths()[2]);
+        assertArrayEquals(new int[]{0, 1, 1, 2, 2, 2, 2}, lcsLengthsAndSolutions.getLengths()[3]);
+        assertArrayEquals(new int[]{0, 1, 1, 2, 2, 3, 3}, lcsLengthsAndSolutions.getLengths()[4]);
+        assertArrayEquals(new int[]{0, 1, 2, 2, 2, 3, 3}, lcsLengthsAndSolutions.getLengths()[5]);
+        assertArrayEquals(new int[]{0, 1, 2, 2, 3, 3, 4}, lcsLengthsAndSolutions.getLengths()[6]);
+        assertArrayEquals(new int[]{0, 1, 2, 2, 3, 4, 4}, lcsLengthsAndSolutions.getLengths()[7]);
 
-        assertArrayEquals(new byte[] {0, 0, 0, 0, 0, 0, 0}, lcsLengths.getChosenSolutions()[0]);
-        assertArrayEquals(new byte[] {0, 2, 2, 2, 1, 3, 1}, lcsLengths.getChosenSolutions()[1]);
-        assertArrayEquals(new byte[] {0, 1, 3, 3, 2, 1, 3}, lcsLengths.getChosenSolutions()[2]);
-        assertArrayEquals(new byte[] {0, 2, 2, 1, 3, 2, 2}, lcsLengths.getChosenSolutions()[3]);
-        assertArrayEquals(new byte[] {0, 1, 2, 2, 2, 1, 3}, lcsLengths.getChosenSolutions()[4]);
-        assertArrayEquals(new byte[] {0, 2, 1, 2, 2, 2, 2}, lcsLengths.getChosenSolutions()[5]);
-        assertArrayEquals(new byte[] {0, 2, 2, 2, 1, 2, 1}, lcsLengths.getChosenSolutions()[6]);
-        assertArrayEquals(new byte[] {0, 1, 2, 2, 2, 1, 2}, lcsLengths.getChosenSolutions()[7]);
+        assertArrayEquals(new int[]{0, 0, 0, 0, 0, 0, 0}, lcsLengthsAndSolutions.getChosenSolutions()[0]);
+        assertArrayEquals(new int[]{0, 2, 2, 2, 1, 3, 1}, lcsLengthsAndSolutions.getChosenSolutions()[1]);
+        assertArrayEquals(new int[]{0, 1, 3, 3, 2, 1, 3}, lcsLengthsAndSolutions.getChosenSolutions()[2]);
+        assertArrayEquals(new int[]{0, 2, 2, 1, 3, 2, 2}, lcsLengthsAndSolutions.getChosenSolutions()[3]);
+        assertArrayEquals(new int[]{0, 1, 2, 2, 2, 1, 3}, lcsLengthsAndSolutions.getChosenSolutions()[4]);
+        assertArrayEquals(new int[]{0, 2, 1, 2, 2, 2, 2}, lcsLengthsAndSolutions.getChosenSolutions()[5]);
+        assertArrayEquals(new int[]{0, 2, 2, 2, 1, 2, 1}, lcsLengthsAndSolutions.getChosenSolutions()[6]);
+        assertArrayEquals(new int[]{0, 1, 2, 2, 2, 1, 2}, lcsLengthsAndSolutions.getChosenSolutions()[7]);
+    }
+
+    @Test
+    void lcsLengthsMemoized () {
+        String sequence1 = "ABCBDAB";
+        String sequence2 = "BDCABA";
+
+        LongestCommonSubsequence.LcsLengthsAndSolutions lcsLengthsAndSolutions = LongestCommonSubsequence.lcsLengthsMemoized(sequence1, sequence2);
+        System.out.println(lcsLengthsAndSolutions.toLengthsTable());
+        System.out.println(lcsLengthsAndSolutions.toChosenSolutionsTable());
+
+        String lcs = LongestCommonSubsequence.constructLcs(lcsLengthsAndSolutions, sequence1, sequence1.length(), sequence2.length());
+        assertEquals("BCBA", lcs);
     }
 
     @Test
     void constructLcs() {
         String sequence1 = "ABCBDAB";
         String sequence2 = "BDCABA";
-        LongestCommonSubsequence.LcsLengths lcsLengths = LongestCommonSubsequence.lcsLength(sequence1, sequence2);
+        LongestCommonSubsequence.LcsLengthsAndSolutions lcsLengthsAndSolutions = LongestCommonSubsequence.lcsLength(sequence1, sequence2);
 
-        String lcs = LongestCommonSubsequence.constructLcs(lcsLengths, sequence1, sequence1.length(), sequence2.length());
+        String lcs = LongestCommonSubsequence.constructLcs(lcsLengthsAndSolutions, sequence1, sequence1.length(), sequence2.length());
+        assertEquals("BCBA", lcs);
+    }
+
+    @Test
+    void constructLcsShort() {
+        String sequence1 = "ABCBDAB";
+        String sequence2 = "BDCABA";
+        int[][] lengths = LongestCommonSubsequence.lcsLength(sequence1, sequence2).getLengths();
+
+        String lcs = LongestCommonSubsequence.constructLcsShort(lengths, sequence1, sequence2);
         assertEquals("BCBA", lcs);
     }
 
@@ -56,5 +79,4 @@ class LongestCommonSubsequenceTest {
         assertEquals("", LongestCommonSubsequence.findLcs("", "BDCABA"));
         assertEquals("", LongestCommonSubsequence.findLcs("", ""));
     }
-
 }
