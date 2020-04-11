f57fde5538311bca2ce3a1b3feb3efd611caacb8
Alexey Lisyutenko <firely3000@gmail.com>
2020-04-11 03:02:55 +0300

    Finish LongestMonotonicallyIncreasingSubsequence

--- src/main/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestMonotonicallyIncreasingSubsequence.java
+++ src/main/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestMonotonicallyIncreasingSubsequence.java
@@ -1,47 +1,14 @@
 package ru.alexeylisyutenko.cormen.chapter15.lcs;
 
-import com.jakewharton.fliptables.FlipTable;
 import lombok.Value;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
-
-import static ru.alexeylisyutenko.helper.Helpers.print2dArrayAsTable;
-
-// TODO: Fix all!
 
 /**
  * Exercise 15.4-5.
  */
 public final class LongestMonotonicallyIncreasingSubsequence {
     private LongestMonotonicallyIncreasingSubsequence() {
-    }
-
-    public static void printSubset(int[] sequence, int index, String subset) {
-        if (index == sequence.length) {
-            System.out.println("{" + subset + "}");
-        } else {
-            // Current element is in subset.
-            printSubset(sequence, index + 1, subset.isEmpty() ? subset + sequence[index] : subset + ", " + sequence[index]);
-
-            // Current element is not in a subset.
-            printSubset(sequence, index + 1, subset);
-        }
-    }
-
-    public static void printSubsetWithLowerBound(int[] sequence, int index, String subset, int lowerBound) {
-        if (index == sequence.length) {
-            System.out.println("{" + subset + "}");
-        } else {
-            // Current element is in subset.
-            if (sequence[index] >= lowerBound) {
-                printSubsetWithLowerBound(sequence, index + 1, subset.isEmpty() ? subset + sequence[index] : subset + ", " + sequence[index], sequence[index]);
-            }
-
-            // Current element is not in a subset.
-            printSubsetWithLowerBound(sequence, index + 1, subset, lowerBound);
-        }
     }
 
     /**
@@ -69,44 +36,86 @@ public final class LongestMonotonicallyIncreasingSubsequence {
         return Math.max(l1, l2);
     }
 
+    /**
+     * This method finds the length of the longestMonotonically increasing subsequence.
+     *
+     * @param sequence sequence of integers
+     * @return the length of the longestMonotonically increasing subsequence
+     */
     public static int lengthOnlyDynamic(int[] sequence) {
         Objects.requireNonNull(sequence, "sequence cannot be null");
+        int[][] lengths = findLMISLengths(sequence).getLengths();
+        return lengths[0][0];
+    }
 
-        int size = sequence.length;
-        if (size == 0) {
-            return 0;
+    /**
+     * Returns the longestMonotonically increasing subsequence.
+     *
+     * @param sequence sequence of integers
+     * @return the longestMonotonically increasing subsequence
+     */
+    public static int[] findLongestMonotonicallyIncreasingSubsequence(int[] sequence) {
+        Objects.requireNonNull(sequence, "sequence cannot be null");
+        LMISLengthsAndDirections lengthsAndDirections = findLMISLengths(sequence);
+        return constructOptimalSolution(sequence, lengthsAndDirections);
+    }
+
+    public static int[] constructOptimalSolution(int[] sequence, LMISLengthsAndDirections lmisLengthsAndDirections) {
+        int optimalSolutionSize = lmisLengthsAndDirections.getLengths()[0][0];
+        int[][] directions = lmisLengthsAndDirections.getDirections();
+
+        int[] optimalSolution = new int[optimalSolutionSize];
+        int optimalSolutionIndex = 0;
+
+        int i = 0;
+        int j = 0;
+        while (i < sequence.length) {
+            if (directions[i][j] == 1) {
+                optimalSolution[optimalSolutionIndex++] = sequence[i];
+                j = i + 1;
+            }
+            i++;
         }
+
+        return optimalSolution;
+    }
+
+    public static LMISLengthsAndDirections findLMISLengths(int[] sequence) {
+        int size = sequence.length;
 
         // Add a special value to the front of the sequence.
         int[] modifiedSequence = new int[size + 1];
         modifiedSequence[0] = Integer.MIN_VALUE;
         System.arraycopy(sequence, 0, modifiedSequence, 1, size);
 
-        //
-        int[][] lengths = new int[size + 1][size];
-
-        // Initialize the last raw.
-        for (int j = 0; j < size; j++) {
-            lengths[size][j] = modifiedSequence[size] < modifiedSequence[j] ? 0 : 1;
-        }
-
-        print2dArrayAsTable(lengths);
-
-        for (int i = size - 1; i >= 1; i--) {
+        // Use bottom-up approach to find the solution.
+        int[][] lengths = new int[size + 1][size + 1];
+        int[][] directions = new int[size + 1][size + 1];
+        for (int i = size; i >= 1; i--) {
             for (int j = i - 1; j >= 0; j--) {
                 int l1 = Integer.MIN_VALUE;
                 if (modifiedSequence[i] >= modifiedSequence[j]) {
-                    l1 = lengths[i + 1][i] + 1;
+                    l1 = lengths[i][i] + 1;
                 }
-                int l2 = lengths[i + 1][j];
-                lengths[i][j] = Math.max(l1, l2);
+                int l2 = lengths[i][j];
+
+                if (l1 >= l2) {
+                    lengths[i - 1][j] = l1;
+                    directions[i - 1][j] = 1;
+                } else {
+                    lengths[i - 1][j] = l2;
+                    directions[i - 1][j] = 2;
+                }
             }
         }
 
-        print2dArrayAsTable(lengths);
+        return new LMISLengthsAndDirections(lengths, directions);
+    }
 
-
-        return lengths[1][0];
+    @Value
+    public static class LMISLengthsAndDirections {
+        int[][] lengths;
+        int[][] directions;
     }
 
 }
--- src/test/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestMonotonicallyIncreasingSubsequenceTest.java
+++ src/test/java/ru/alexeylisyutenko/cormen/chapter15/lcs/LongestMonotonicallyIncreasingSubsequenceTest.java
@@ -1,29 +1,24 @@
 package ru.alexeylisyutenko.cormen.chapter15.lcs;
 
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.alexeylisyutenko.cormen.chapter15.lcs.LongestMonotonicallyIncreasingSubsequence.*;
 
 class LongestMonotonicallyIncreasingSubsequenceTest {
 
     @Test
-    void printSubsetDemo() {
-        LongestMonotonicallyIncreasingSubsequence.printSubset(new int[]{1, 9, 2, 3}, 0, "");
-        System.out.println();
-        System.out.println();
-        LongestMonotonicallyIncreasingSubsequence.printSubsetWithLowerBound(new int[]{1, 9, 2, 3}, 0, "", Integer.MIN_VALUE);
-        System.out.println();
-        System.out.println();
-        LongestMonotonicallyIncreasingSubsequence.printSubsetWithLowerBound(new int[]{1, 2}, 0, "", Integer.MIN_VALUE);
-    }
-
-    @Test
     void lengthOnly() {
-        assertEquals(0, LongestMonotonicallyIncreasingSubsequence.lengthOnlyRecursive(new int[]{}));
-        assertEquals(1, LongestMonotonicallyIncreasingSubsequence.lengthOnlyRecursive(new int[]{6}));
-        assertEquals(2, LongestMonotonicallyIncreasingSubsequence.lengthOnlyRecursive(new int[]{1, 2}));
-        assertEquals(1, LongestMonotonicallyIncreasingSubsequence.lengthOnlyRecursive(new int[]{2, 1}));
-        assertEquals(3, LongestMonotonicallyIncreasingSubsequence.lengthOnlyRecursive(new int[]{1, 9, 2, 3}));
+        assertEquals(0, lengthOnlyRecursive(new int[]{}));
+        assertEquals(1, lengthOnlyRecursive(new int[]{6}));
+        assertEquals(2, lengthOnlyRecursive(new int[]{1, 2}));
+        assertEquals(1, lengthOnlyRecursive(new int[]{2, 1}));
+        assertEquals(3, lengthOnlyRecursive(new int[]{1, 9, 2, 3}));
     }
 
     @Test
@@ -35,10 +30,36 @@ class LongestMonotonicallyIncreasingSubsequenceTest {
         assertEquals(3, LongestMonotonicallyIncreasingSubsequence.lengthOnlyDynamic(new int[]{1, 9, 2, 3}));
     }
 
+    @RepeatedTest(1000)
+    void lengthOnlyDynamicRandomized() {
+        int size = RandomUtils.nextInt(0, 50);
+        int[] sequence = new int[size];
+        for (int i = 0; i < size; i++) {
+            sequence[i] = RandomUtils.nextInt(0, 1000);
+        }
+        assertEquals(lengthOnlyRecursive(sequence), LongestMonotonicallyIncreasingSubsequence.lengthOnlyDynamic(sequence));
+    }
+
     @Test
-    void lengthOnlyDynamicDemo() {
-        System.out.println(LongestMonotonicallyIncreasingSubsequence.lengthOnlyDynamic(new int[]{1, 9, 2, 3}));
-        System.out.println(LongestMonotonicallyIncreasingSubsequence.lengthOnlyDynamic(new int[]{}));
+    void testFindLongestMonotonicallyIncreasingSubsequence() {
+       assertArrayEquals(new int[] {}, findLongestMonotonicallyIncreasingSubsequence(new int[]{}));
+       assertArrayEquals(new int[] {6}, findLongestMonotonicallyIncreasingSubsequence(new int[]{6}));
+       assertArrayEquals(new int[] {1, 2}, findLongestMonotonicallyIncreasingSubsequence(new int[]{1, 2}));
+       assertArrayEquals(new int[] {2}, findLongestMonotonicallyIncreasingSubsequence(new int[]{2, 1}));
+       assertArrayEquals(new int[] {1, 2, 3}, findLongestMonotonicallyIncreasingSubsequence(new int[]{1, 9, 2, 3}));
+    }
+
+    @Test
+    void findLongestMonotonicallyIncreasingSubsequenceDemo() {
+        int size = RandomUtils.nextInt(0, 10);
+        int[] sequence = new int[size];
+        for (int i = 0; i < size; i++) {
+            sequence[i] = RandomUtils.nextInt(0, 100);
+        }
+        System.out.println(Arrays.toString(sequence));
+
+        int[] solution = findLongestMonotonicallyIncreasingSubsequence(sequence);
+        System.out.println(Arrays.toString(solution));
     }
 
 }
